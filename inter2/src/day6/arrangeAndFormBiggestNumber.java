320a7d27a65b62ffd9685d091bbbeac8a002959c
Gaurav <gauurav37@gmail.com>
2018-06-22 09:56:39 +0530

    Questions on Searching and Sorting

--- inter2/src/day6/arrangeAndFormBiggestNumber.java
+++ inter2/src/day6/arrangeAndFormBiggestNumber.java
@@ -1,5 +1,20 @@
 package day6;
 
-public class arrangeAndFormBiggestNumber {
+import java.util.Scanner;
 
+public class arrangeAndFormBiggestNumber {
+	public void printLargest(int[] arr) {
+		
+	}
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[sc.nextInt()];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		sc.close();
+		arrangeAndFormBiggestNumber a = new arrangeAndFormBiggestNumber();
+		a.printLargest(arr);
+	}
 }
--- inter2/src/day6/rearrngeArrayWithoutSpace.java
+++ inter2/src/day6/rearrngeArrayWithoutSpace.java
@@ -1,10 +1,37 @@
 package day6;
 
 public class rearrngeArrayWithoutSpace {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		rearrange(arr);
 	}
 
+	public static void rearrange(int[] arr) {
+		int left = 0;
+		int right = arr.length - 1;
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		for (int i = 0; i < arr.length; i++) { // to find out a value we can mod
+			// elements with , we can also choose a random number greater than
+			// max value
+			max = Math.max(max, arr[i]);
+			min = Math.min(min, arr[i]);
+		}
+		max += min;
+		int i = 0;
+		while (i < arr.length) {
+			if (i % 2 == 0) {
+				arr[i] = (arr[right] % max) * max + arr[i];
+				right--;
+				i++;
+			} else {
+				arr[i] = (arr[left] % max) * max + arr[i];
+				left++;
+				i++;
+			}
+		}
+		for (int x : arr) {
+			System.out.print(x / max + " ");
+		}
+	}
 }
