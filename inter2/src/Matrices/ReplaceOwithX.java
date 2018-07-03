1d1956ce0740bb03d5923bdbae798b0d7be65527
Gaurav <gauurav37@gmail.com>
2018-07-03 17:23:06 +0530

    new Problems on matrices

--- inter2/src/Matrices/ReplaceOwithX.java
+++ inter2/src/Matrices/ReplaceOwithX.java
@@ -3,47 +3,62 @@ package Matrices;
 import java.util.Scanner;
 
 public class ReplaceOwithX {
-	public static void replaceOwithX(char[][] mat, boolean[][] arr, int row, int col) {
-		if (row + 1 < mat.length && mat[row + 1][col] == 'O' && arr[row + 1][col] == false) {
-			arr[row + 1][col] = true;
-			replaceOwithX(mat, arr, row + 1, col);
+		public static void replaceOwithX(char[][] mat, boolean[][] arr, int row, int col) {
+			if (mat[row][col] == 'O') {
+				mat[row][col] = 'I';
+			}
+			if (row + 1 < mat.length && mat[row + 1][col] == 'O' && arr[row + 1][col] == false) {
+				arr[row + 1][col] = true;
+				replaceOwithX(mat, arr, row + 1, col);
+			}
+			if (row - 1 >= 0 && mat[row - 1][col] == 'O' && arr[row - 1][col] == false) {
+				arr[row - 1][col] = true;
+				replaceOwithX(mat, arr, row - 1, col);
+			}
+			if (col + 1 < mat[0].length && mat[row][col + 1] == 'O' && arr[row][col + 1] == false) {
+				arr[row][col + 1] = true;
+				replaceOwithX(mat, arr, row, col + 1);
+			}
+			if (col - 1 >= 0 && mat[row][col - 1] == 'O' && arr[row][col - 1] == false) {
+				arr[row][col - 1] = true;
+				replaceOwithX(mat, arr, row, col - 1);
+			}
 		}
-		if (row - 1 >= 0 && mat[row - 1][col] == 'O' && arr[row - 1][col] == false) {
-			arr[row - 1][col] = true;
-			replaceOwithX(mat, arr, row - 1, col);
+	
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			char[][] mat = new char[sc.nextInt()][sc.nextInt()];
+			for (int i = 0; i < mat.length; i++) {
+				for (int j = 0; j < mat[0].length; j++) {
+					mat[i][j] = sc.next().charAt(0);
+				}
+			}
+			sc.close();
+			boolean[][] arr = new boolean[mat.length][mat[0].length];
+			for (int i = 0; i < mat.length; i++) {
+				for (int j = 0; j < mat[0].length; j++) {
+					if (i == 0 || j == 0 || i == mat.length - 1 || j == mat[0].length - 1) {
+						if (mat[i][j] == 'O')
+							replaceOwithX(mat, arr, i, j);
+					}
+				}
+			}
+			for (int i = 0; i < mat.length; i++) {
+				for (int j = 0; j < mat[0].length; j++) {
+					if (mat[i][j] == 'O') {
+						mat[i][j] = 'X';
+					}
+				}
+			}
+			for (int i = 0; i < mat.length; i++) {
+				for (int j = 0; j < mat[0].length; j++) {
+					if (mat[i][j] == 'I') {
+						mat[i][j] = 'O';
+					}
+					System.out.print(mat[i][j] + " ");
+				}
+				System.out.println();
+			}
 		}
-		if (col + 1 < mat[0].length && mat[row][col + 1] == 'O' && arr[row][col + 1] == false) {
-			arr[row][col + 1] = true;
-			replaceOwithX(mat, arr, row, col + 1);
-		}
-		if (col - 1 >= 0 && mat[row][col - 1] == 'O' && arr[row][col - 1] == false) {
-			arr[row][col - 1] = true;
-			replaceOwithX(mat, arr, row, col - 1);
-		}
+	
 	}
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		char[][] mat = new char[sc.nextInt()][sc.nextInt()];
-		for (int i = 0; i < mat.length; i++) {
-			for (int j = 0; j < mat[0].length; j++) {
-				mat[i][j] = sc.next().charAt(0);
-			}
-		}
-		sc.close();
-		boolean[][] arr = new boolean[mat.length][mat[0].length];
-		for (int i = 0; i < mat.length; i++) {
-			for (int j = 0; j < mat[0].length; j++) {
-				if (i == 0 || j == 0 || i == mat.length - 1 || j == mat[0].length - 1)
-					replaceOwithX(mat, arr, i, j);
-			}
-		}
-		for (int i = 0; i < mat.length; i++) {
-			for (int j = 0; j < mat[0].length; j++) {
-				System.out.print(mat[i][j]+" ");
-			}
-			System.out.println();
-		}
-	}
-
-}
